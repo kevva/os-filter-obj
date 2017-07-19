8c354f92be9042dbf7ced70887ea413990bbf90e
simonepri <simone.pri@hotmail.it>
2017-07-19 16:18:28 +0200

    Add tests for the keep option

--- test.js
+++ test.js
@@ -23,3 +23,25 @@ test('filter an array of objects to a specific os', t => {
 	t.is(osFilterObj(arr)[1].foo, process.platform);
 	t.is(osFilterObj(arr)[1].os, undefined);
 });
+
+test('filter an array of objects to a specific os without alter the objects', t => {
+	var arr = [{
+		foo: 'all'
+	}, {
+		foo: 'linux',
+		os: 'linux'
+	}, {
+		foo: 'darwin',
+		os: 'darwin'
+	}, {
+		foo: 'win32',
+		os: 'win32'
+	}, {
+		foo: 'arm',
+		os: 'arm'
+	}];
+
+	t.is(osFilterObj(arr, {keep: true}).length, 2);
+	t.is(osFilterObj(arr, {keep: true})[0].os, undefined);
+	t.is(osFilterObj(arr, {keep: true})[1].os, process.platform);
+});
