099950a93fa458a3f657059599bee6977e02564a
simonepri <simone.pri@hotmail.it>
2017-07-19 16:10:50 +0200

    Add option to allow object to remain unaltered after the filter

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 var arch;
 var platform;
 
-module.exports = function (arr) {
+module.exports = function (arr, opts) {
 	if (!arr || !arr.length) {
 		return [];
 	}
 
 	platform = platform || process.platform;
 	arch = arch || require('arch')();
+
+  if (opts && opts.keep) {
+		return arr.filter(function (obj) {
+			if ((obj.os === platform || !obj.os) && (obj.arch === arch || !obj.arch)) {
+			  return obj;
+			}
+		});
+	}
 
 	return arr.filter(function (obj) {
 		if (obj.os === platform && obj.arch === arch) {
