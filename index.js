fe8508e2201ca7e64e5211a4b8fda2bd8553d6d5
Igor Gassmann <igor@igassmann.me>
2021-11-01 16:32:22 +0100

    Add support for npm prebuild config env vars

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = input => {
-	const arch = require('arch')();
+	const platform = process.env.npm_config_platform || process.platform;
+	const arch = process.env.npm_config_arch || require('arch')();
+
 	const check = (bool, key, val) => (!bool || !key || key === val);
 
-	return input.filter(x => [process.platform, arch].every((y, i) => check(i === 0, x.os, y) && check(i === 1, x.arch, y)));
+	return input.filter(x => [platform, arch].every((y, i) => check(i === 0, x.os, y) && check(i === 1, x.arch, y)));
 };
